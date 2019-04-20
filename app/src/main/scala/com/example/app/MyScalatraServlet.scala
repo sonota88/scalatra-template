6ea92da121c55d122e27867ad67447434f6cc934
sonota88 <yosiot8753@gmail.com>
2019-04-20 17:20:53 +0900

    return json

--- app/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ app/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -5,7 +5,15 @@ import org.scalatra._
 class MyScalatraServlet extends ScalatraServlet {
 
   get("/") {
-    views.html.hello()
+
+    contentType = "application/json"
+
+    // views.html.hello()
+    """
+{
+  "a": 123456789
+}
+    """
   }
 
 }
