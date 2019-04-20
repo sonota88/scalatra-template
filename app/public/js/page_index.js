2a6c5494db5688e5e690a73218fdaffc8a232ced
sonota88 <yosiot8753@gmail.com>
2019-04-20 19:04:28 +0900

    Render page

--- app/public/js/page_index.js
+++ app/public/js/page_index.js
@@ -1,1 +1,28 @@
-console.log(Date.now());
+class View {
+  static render(state){
+    return TreeBuilder.build(h=>
+      h("div", {}, "TODO")
+    );
+  }
+}
+
+class Page {
+  constructor(){
+    this.state = {};
+  }
+
+	init(){
+    console.log("init");
+    this.render();
+  }
+
+  getTitle(){
+    return "scalatra-template index";
+  }
+
+  render(){
+    $("#main").empty().append(View.render(this.state));
+  }
+}
+	
+__g.ready(new Page());
