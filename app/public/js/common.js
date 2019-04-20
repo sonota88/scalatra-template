eb485bfa34c1250ff45f76e2a0b904a4102b09ef
sonota88 <yosiot8753@gmail.com>
2019-04-20 18:56:42 +0900

    Add common.js

---
+++ app/public/js/common.js
@@ -0,0 +1,74 @@
+function puts(... args){
+  console.log.apply(console, args);
+}
+
+const __g = {
+  api: function(method, path, data, fnOk, fnNg){
+    var _data = {
+      _method: method.toUpperCase()
+      ,_params: JSON.stringify(data)
+    };
+    $.post(path, _data, (data)=>{
+      if(data.errors.length > 0){
+        fnNg(data.errors);
+        return;
+      }
+      fnOk(data.result);
+    });
+  },
+
+  api_v2: (method, path, data, fnOk, fnNg)=>{
+    const req = new Request(path);
+
+    const fd = new FormData();
+    fd.append("_method", method.toUpperCase());
+    fd.append("_params", JSON.stringify(data));
+
+    fetch(req, {
+      method: 'POST',
+      body: fd,
+      credentials: 'include', // cookie をリクエストに含める
+    }).then((res)=>{
+      if (res.ok) {
+        puts("res.ok == true", res);
+      } else {
+        puts("res.ok != true", res);
+      }
+      return res.json();
+    }).then((resData)=>{
+      if (resData.errors.length > 0) {
+        fnNg(resData.errors);
+        return;
+      }
+      fnOk(resData.result);
+    }).catch((err)=>{
+      puts(err);
+    });
+  },
+
+  guard: ()=>{
+    $("#guard_layer").show();
+  },
+
+  unguard: ()=>{
+    setTimeout(()=>{
+      $("#guard_layer").fadeOut(100);
+    }, 100);
+  },
+
+  printApiErrors: (es)=>{
+    es.forEach((e, i)=>{
+      puts(`-------- error ${i} --------`);
+      puts(e.trace.split("\n").reverse().join("\n"));
+      puts(e.msg);
+    });
+  },
+
+  ready: (page)=>{
+    window.__p = page;
+    document.addEventListener("DOMContentLoaded", ()=>{
+      page.init();
+      document.title = page.getTitle() + " | {app_name}";
+    });
+  }
+};
