5d2bce7ae1e085e5ce71a61cfdd3995005722ae6
sonota88 <yosiot8753@gmail.com>
2019-04-20 19:33:43 +0900

    Use java.nio.file.Paths to join path

--- app/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ app/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,12 +1,15 @@
 package com.example.app
 
 import org.scalatra._
+import java.nio.file.Paths
 
 class MyScalatraServlet extends ScalatraServlet {
 
   def readStaticFile(path:String):String = {
     val myAppDir = sys.env("MY_APP_DIR")
-    Utils.readFile(myAppDir + "/public" + path)
+    Utils.readFile(
+      Paths.get(myAppDir, "public", path).toString()
+    )
   }
 
   get("/") {
