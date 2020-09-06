2cf9f7de2ccb275ee1599021b087c341ca7c5461
sonota88 <yosiot8753@gmail.com>
2020-09-07 06:40:04 +0900

    2020-09-07 06:40:04 format (app/src/main/scala/myapp/JettyLauncher.scala)

--- app/src/main/scala/myapp/JettyLauncher.scala
+++ app/src/main/scala/myapp/JettyLauncher.scala
@@ -1,4 +1,5 @@
 package myapp
+
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.webapp.WebAppContext
@@ -7,7 +8,7 @@ import org.scalatra.servlet.ScalatraListener
 object JettyLauncher {
 
   def main(args: Array[String]) {
-    val port = if(System.getenv("PORT") != null) {
+    val port = if (System.getenv("PORT") != null) {
       System.getenv("PORT").toInt
     } else {
       8080
